0182c9da5ac65a3695859c0039115e99d66d1bb9
Jorakasi <gabor.olah0323@gmail.com>
2020-10-21 14:29:57 +0200

    PalindromePermutation

---
+++ Exercises/src/PalindromePermutation.java
@@ -0,0 +1,49 @@
+public class PalindromePermutation {
+
+    /*
+    Given a string, write a function to check if it is a permutation of a palindrome.
+    A palindrome is a word or phrase that is the same forwards and backwards.
+    A permutation is a rearrangement of letters.
+    The palindrome does not need to be limited to just dictionary words.
+     */
+
+    boolean isPermutationOfPalindrome(String str) {
+        int[] table = buildCharFrequencyTable(str);
+        return checkMaxOneOdd(table);
+    }
+
+    boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
+
+    int getCharNumber(Character c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue('c');
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    int[] buildCharFrequencyTable(String str) {
+        int[] table = new int[Character.getNumericValue('z') -
+                Character.getNumericValue('a') + 1];
+        for (char c : str.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+            }
+        }
+        return table;
+    }
+}
