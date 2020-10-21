80823fd3e48692a609630223ce688dc4fbed8778
Jorakasi <gabor.olah0323@gmail.com>
2020-10-21 09:56:06 +0200

    solution for URLify

--- Exercises/src/URLify.java
+++ Exercises/src/URLify.java
@@ -5,6 +5,25 @@ public class URLify {
     space at the end to hold the additional characters, and that you are given the true length of the string.
      */
 
-    
-
+    void replaceSpaces(char[] str, int length) {
+        int spaceCount = 0, index, i;
+        for (int j = 0; j < length; j++) {
+            if (str[j] == ' ') {
+                spaceCount++;
+            }
+        }
+        index = length + spaceCount * 2;
+        if (length < str.length) str[length] = '\0';
+        for (i = length - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                str[index - 1] = '0';
+                str[index - 2] = '2';
+                str[index - 3] = '%';
+                index = index - 3;
+            } else {
+                str[index - 1] = str[i];
+                index--;
+            }
+        }
+    }
 }
